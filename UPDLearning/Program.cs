511fc07e84df1d3aa7ea965e3a66c6dc93edf4be
qq21 <237041086@qq.com>
2019-04-23 18:26:55 +0800

    TryCatch的处理 s异常捕获  TODO:掉线时检查 列表中是否存在该ip存在就进行 移除，

--- UPDLearning/Program.cs
+++ UPDLearning/Program.cs
@@ -8,10 +8,28 @@ using System.Threading;
 
 namespace UPDLearning
 {
+    class Client
+    {
+        private UdpClient _udpClient;
+
+        public bool isConnect
+        {
+            get
+            {
+                return _udpClient.Client.Connected;
+            }
+        }
+
+
+        public Client(IPEndPoint iPEndPoint)
+        {
+            _udpClient = new UdpClient(iPEndPoint);
+        }
+    }
+
     class Program
     {
         static UdpClient udpServer = new UdpClient(new IPEndPoint(IPAddress.Parse("192.168.1.121"), 7789));
-
         static List<IPEndPoint> Cliens = new List<IPEndPoint>();
        private static byte[] data;
         static void Main(string[] args)
@@ -21,8 +39,8 @@ namespace UPDLearning
 
             //IPEndPoint iPEnd=new IPEndPoint(IPAddress.Parse("192.168.1.122"),7788);
                 
-            Thread t1 = new Thread( Receive);
-            t1.Start();
+            Thread recevieT = new Thread( Receive);
+            recevieT.Start();
 
             //while (true)
             //{
@@ -36,45 +54,62 @@ namespace UPDLearning
         static void Receive()
         {
 
-            IPEndPoint iPEnd = new IPEndPoint(IPAddress.Any, 0);
+        
             Console.WriteLine("服务器启动成功,等待玩家连接");
             while (true)
             {
-               
 
-
-                if (udpServer.Client.Poll(10,SelectMode.SelectRead))
+                if (udpServer.Client.Poll(10, SelectMode.SelectRead))
                 {
 
-               
-                    data = udpServer.Receive(ref iPEnd);
-
-                    string[] datas = Encoding.UTF8.GetString(data).Split('@');
-
-                    //Todo:判断字典中 是否存在该 UDP
-                    if (!Cliens.Contains(iPEnd))
+                    IPEndPoint iPEnd = new IPEndPoint(IPAddress.Any, 0);
+                
+                    try
                     {
-                        Console.WriteLine(iPEnd.Address + ":"+iPEnd.Port+"连接进来了");
+                        data = udpServer.Receive(ref iPEnd);
+                        string[] datas = Encoding.UTF8.GetString(data).Split('@');
 
-                        Cliens.Add(iPEnd);
+                        //Todo:判断字典中 是否存在该 UDP
+                        if (!Cliens.Contains(iPEnd))
+                        {
+                            Console.WriteLine(iPEnd.Address + ":" + iPEnd.Port + "连接进来了");
+
+                            Cliens.Add(iPEnd);
+                        }
+
+                        //TODO: 收到 掉线命令，然后在字典里进行处理 判断是否存在  如果 存在 进行移除;
+                         
+                        string mes = datas[1];
+
+                        Console.WriteLine("从" + iPEnd.Address + "端口:" + iPEnd.Port + "收到了数据：" + mes);
+
+                        // TODO 进行广播 遍历连接进来的客服端
+                        Boracast(data, iPEnd);
+                    }
+                    catch (Exception e)
+                    {
+                        if (e.GetType() == typeof(SocketException))
+                        {
+                            //Console.WriteLine(udpServer.Available);
+                            //Console.WriteLine("断开连接" + iPEnd.Address.ToString());
+                        }
                     }
 
-                    string mes = datas[1];
-                    
-                     Console.WriteLine("从" + iPEnd.Address + "端口:" + iPEnd.Port + "收到了数据：" + mes);
-
-                    // TODO 进行广播 遍历连接进来的客服端
-                    Boracast(data,iPEnd);
                 }
             }
         }
 
+        /// <summary>
+        ///  对 客服端进行 广播， 判断是否处于连接 状态
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="iPEnd"></param>
         static void Boracast(byte [] data,IPEndPoint iPEnd)
         {
             foreach (var client in Cliens)
             {
                 if (client!=iPEnd)
-                {
+                {                       
                     udpServer.Send(data, data.Length, client);
                 }
             
